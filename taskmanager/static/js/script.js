b11f99817a7b22514465b8520754df2915665e84
GenaPlem <shainkill1337@gmail.com>
2023-10-18 13:22:22 +0100

    Add functionality to add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     let modal = document.querySelectorAll('.modal');
-    let modal_close = document.querySelectorAll('.modal-close')
+    let modal_close = document.querySelectorAll('.modal-close');
+    let datepicker = document.querySelectorAll('.datepicker');
+    let selects = document.querySelectorAll('select');
+
     M.Sidenav.init(sidenav);
     M.Modal.init(modal);
     M.Modal.init(modal_close);
+    M.Datepicker.init(datepicker, {
+        format: 'dd mmmm, yyyy',
+        i18n: {done: "Select"}
+    });
+    M.FormSelect.init(selects);
 });
