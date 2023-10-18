f42455c84ef9cc35610cfa1e831227ee6ab23a8f
GenaPlem <shainkill1337@gmail.com>
2023-10-18 12:55:52 +0100

    Add Delete functionality to categories

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
+    let modal = document.querySelectorAll('.modal');
+    let modal_close = document.querySelectorAll('.modal-close')
     M.Sidenav.init(sidenav);
+    M.Modal.init(modal);
+    M.Modal.init(modal_close);
 });
